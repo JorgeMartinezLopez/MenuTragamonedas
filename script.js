9abd8fa0b5f965a1fcf2ef4e04554b6e2643a993
JorgeMartinezLopez <145234797+JorgeMartinezLopez@users.noreply.github.com>
2023-09-16 17:19:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const questionText = "¿Cuál es la probabilidad de que...";
+const questionElement = document.getElementById("question");
+const answerElement = document.getElementById("answer");
+
+function typeWriter(textElement, text, speed, callback) {
+    let i = 0;
+    textElement.innerHTML = ""; // Limpiar el contenido inicial
+    const timer = setInterval(function () {
+        if (i < text.length) {
+            textElement.innerHTML += text.charAt(i);
+            i++;
+        } else {
+            clearInterval(timer);
+            // Llama a la función de devolución de llamada cuando la animación termine
+            if (callback) {
+                callback();
+            }
+        }
+    }, speed);
+}
+
+// Llamar a la función typeWriter para escribir el texto gradualmente
+typeWriter(questionElement, questionText, 50, function () {
+    // El texto de arriba ha terminado de escribirse, ahora puedes mostrar el texto de abajo
+    typeWriter(answerElement, "Logres obtener 3 figuras iguales?", 50);
+});
+
+document.getElementById("playButton").addEventListener("click", function () {
+    // Cambiar la ubicación del navegador al sitio web deseado
+    window.location.href = "https://jorgemartinezlopez.github.io/TragamonedasGeometrico.github.io/"; // Reemplaza con la URL que desees
+});
+
